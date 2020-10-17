9c7568561adf5935be828d6bee3e32b318616d3a
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-10-17 12:47:02 +0900

    ご指摘箇所の修正です

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        validates :nickname,        presence: true
-        #validates :first_name,      presence: true
-        #validates :last_name,       presence: true
-        #validates :first_name_kana, presence: true
-        #validates :last_name_kana,  presence: true
-        validates :birth_day,       presence: true
+        
 
         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
         validates_format_of :password, with: PASSWORD_REGEX
@@ -21,12 +16,9 @@ class User < ApplicationRecord
           validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}          
           validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/}
           validates :last_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/}
+          validates :nickname
+          validates :birth_day
         end
  end
 
- #with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-  #validates :first_name
-  #validates :last_name
-  #validates :first_name_kana
- # validates :last_name_kana
-#end
+ 
