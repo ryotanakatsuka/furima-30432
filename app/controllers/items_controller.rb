4bb3fb62d4d25f63b7b19759617ab042a7951247
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-10-22 10:17:36 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_top,only:[:edit]
+  before_action :authenticate_user!, except:[:index,:show]
   def index
     
     @items=Item.includes(:user).order("created_at DESC")
@@ -22,9 +24,32 @@ class ItemsController < ApplicationController
     @item=Item.find(params[:id])
   end
 
+  def edit
+    @item=Item.find(params[:id])
+  end
+
+
+  def update
+      @item=Item.find(params[:id])
+      
+      if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit
+        #binding.pry
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:name,:text,:condition_id,:delivery_source_id,:price,:image,:genre_id,:shipping_charges_id,:delivery_date_id,).merge(user_id: current_user.id)
+  end
+
+  def move_to_top
+     @item=Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to root_path
+      
+    end
   end
 end
