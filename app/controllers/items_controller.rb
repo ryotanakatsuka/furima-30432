0b0ddf766be921439fac79964c566ed8223b84a0
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-10-22 16:21:24 +0900

    ご指摘の箇所修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :move_to_top,        only:[:edit]
   before_action :authenticate_user!, except:[:index,:show]
   before_action :set_item,           only:[:edit,:show,:update,:destroy]
+  before_action :move_to_top,        only:[:edit,:destroy]
   def index
      @items=Item.includes(:user).order("created_at DESC")
   end
